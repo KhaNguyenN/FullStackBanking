d4a06bc659b5d8aec14ad3411f09489c055bf921
Kha <KhaNguyen5000@gmail.com>
2021-07-23 17:58:28 -0600

    Changed env variables

--- Project-2/src/main/java/com/revature/utils/ConnectionUtil.java
+++ Project-2/src/main/java/com/revature/utils/ConnectionUtil.java
@@ -41,11 +41,14 @@ public class ConnectionUtil {
 		
 //		//We need to provide our database credentials
 //		//We'll hardcode them for now, but we'll see how to hide this username/password in environment variables
-		String url = "jdbc:postgresql://javafs210601.c9hxbolytvxu.us-east-2.rds.amazonaws.com:5432/postgres?currentSchema=Bank";
-        String username = "postgres"; 
-        String password = "password"; //this will be whatever password you set in postgres (or RDS if you're using that)
-//									  //hopefully you just left it as password...
+//		String url = "jdbc:postgresql://javafs210601.c9hxbolytvxu.us-east-2.rds.amazonaws.com:5432/postgres?currentSchema=Bank";
+//        String username = "postgres"; 
+//        String password = "password"; //this will be whatever password you set in postgres (or RDS if you're using that)
+////									  //hopefully you just left it as password...
 		
+		String url = System.getenv("url");
+		String password = System.getenv("password");
+		String username = System.getenv("username");
 		
 		//I've successfully hidden my DB credentials in my environment variables
 		//run -> run configurations -> environment -> add the environment variable key/pairs you want
