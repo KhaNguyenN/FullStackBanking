961515fea071d9f4566e7647841a82e0bb6e691b
david.c.knutson <david.c.knutson@revature.net>
2021-08-03 12:01:45 -0700

    Savings changes

--- Project-2/src/main/java/com/revature/controllers/SavingsController.java
+++ Project-2/src/main/java/com/revature/controllers/SavingsController.java
@@ -87,7 +87,7 @@ public class SavingsController {
 
 		}
 		
-		
+	}
 
 		public void savingsToChecking(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException
 		{
@@ -437,4 +437,4 @@ public class SavingsController {
 	
 	
 	
-}
+
